19f7f0e1811d6107b5daba88a5ec3044926cf3d3
Andre Diniz <andreg.diniz97@hotmail.com>
2022-04-02 21:24:00 -0300

    Start of documentation using swagger.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerFile from './swagger.json';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log('Server is running!'));
