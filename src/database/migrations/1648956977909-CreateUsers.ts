fbee6160aadb2eb692ad001480b9fec95c7caea7
Andre Diniz <andreg.diniz97@hotmail.com>
2022-04-03 01:20:20 -0300

    Migration of user table created with typeorm.

---
+++ src/database/migrations/1648956977909-CreateUsers.ts
@@ -0,0 +1,68 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1648956977909 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'updateDateTime',
+            type: 'timestamp',
+          },
+          {
+            name: 'personalId',
+            type: 'varchar',
+          },
+          {
+            name: 'brandName',
+            type: 'varchar',
+          },
+          {
+            name: 'civilName',
+            type: 'varchar',
+          },
+          {
+            name: 'socialName',
+            type: 'varchar',
+          },
+          {
+            name: 'birthDate',
+            type: 'varchar',
+          },
+          {
+            name: 'maritalStatusCode',
+            type: 'varchar',
+          },
+          {
+            name: 'maritalStatusAdditionalInfo',
+            type: 'varchar',
+          },
+          {
+            name: 'sex',
+            type: 'varchar',
+          },
+          {
+            name: 'companyCnpj',
+            type: 'varchar',
+            isArray: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
